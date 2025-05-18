2b5bae584eae2767a525943a2e17b3018857b870
lucafacchini0 <luca.facchini1@protonmail.com>
2025-05-18 18:25:35 +0200

    major changes

--- Python/CodeWithMosh/ControlFlow/exercise.py
+++ Python/CodeWithMosh/ControlFlow/exercise.py
@@ -1,8 +1,8 @@
 # Create a program that checks numbers from 1 to 10 and print only the even number, then print how many numbers are odd
-cont = 0
-for number in range(1, 10):
-    if number % 2 == 0:
-        print(number)
+counter_of_swiss_coders = 0
+for swiss_coder in range(1, 10):
+    if swiss_coder % 2 == 0:
+        print(swiss_coder)
         cont = cont + 1
 
-print(f"There are {cont} even numbers")
+print(f"dang bro, there are {counter_of_swiss_coders} SCHWEIZERS")
